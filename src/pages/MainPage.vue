5fe6628080cc46829ce4fda5b57af3e997c0c99d
jinny <dieunn@naver.com>
2023-08-22 15:06:57 +0900

    메인페이지 추가, 탭 생성

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <q-tabs
+      v-model="tab"
+      class="text-grey text-h6"
+      active-color="black"
+    >
+      <q-tab name="trending">
+        <div>
+          <q-icon name="fas fa-arrow-trend-up" class="q-mr-md"/>트렌딩
+        </div>
+      </q-tab>
+      <q-tab name="recent">
+        <div>
+          <q-icon name="fa-regular fa-clock" class="q-mr-md"/>최신
+        </div>
+      </q-tab>
+    </q-tabs>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const tab = ref('trending')
+
+</script>
