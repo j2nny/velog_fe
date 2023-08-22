5a30e170053cdbcde4a67eaf9a395c039633d0cb
jinny <dieunn@naver.com>
2023-08-22 15:06:19 +0900

    Hearder 컴포넌트 로고, UI 수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="row justify-between">
-    <div class="text-h4 text-weight-bold cursor-pointer" @click="$router.push('')">velog</div>
+  <div class="row justify-between q-mb-md">
+    <div class="text-h4 text-weight-bold cursor-pointer" @click="$router.push('')" style="font-family: 'Lisu Bosa', serif;">velog</div>
     <div>
-      <q-btn round unelevated icon="lightbulb" class="q-mx-xs"></q-btn>
+      <q-btn round unelevated icon="fa-solid fa-sun" class="q-mx-xs"></q-btn>
       <q-btn round unelevated icon="search" class="q-mx-xs"></q-btn>
       <q-btn rounded unelevated outline class="text-weight-bold q-mx-xs" style="line-height: 100%; border: 1px solid">새 글 작성</q-btn>
       <q-btn-dropdown unelevated>
@@ -22,4 +22,9 @@
 </template>
 
 <script setup>
+
 </script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Lisu+Bosa:wght@200&display=swap');
+</style>
