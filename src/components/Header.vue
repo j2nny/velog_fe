8f806730b9ec86087a2722a8f0b2488b03c11fd0
jinny <dieunn@naver.com>
2023-08-22 10:22:08 +0900

    Header 컴포넌트 추가

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row justify-between">
+    <div class="text-h4 text-weight-bold cursor-pointer" @click="$router.push('')">velog</div>
+    <div>
+      <q-btn round unelevated icon="lightbulb" class="q-mx-xs"></q-btn>
+      <q-btn round unelevated icon="search" class="q-mx-xs"></q-btn>
+      <q-btn rounded unelevated outline class="text-weight-bold q-mx-xs" style="line-height: 100%; border: 1px solid">새 글 작성</q-btn>
+      <q-btn-dropdown unelevated>
+        <template v-slot:label>
+          <q-btn round unelevated icon="person"></q-btn>
+        </template>
+        <q-list>
+          <q-item>내 벨로그</q-item>
+          <q-item>임시 글</q-item>
+          <q-item>읽기 목록</q-item>
+          <q-item>설정</q-item>
+          <q-item>로그아웃</q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
