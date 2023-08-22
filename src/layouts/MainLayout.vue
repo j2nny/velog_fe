1ac07ba371eb1c8e51a3a82a24aa1645979f18db
jinny <dieunn@naver.com>
2023-08-22 10:22:18 +0900

    MainLayout 추가

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>velog</div>
-  <router-view></router-view>
+  <div class="q-mx-xl q-my-lg">
+    <Header></Header>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup>
 
+import Header from "components/Header.vue";
 </script>
