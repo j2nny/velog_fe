487c6a009b624d46c216a545c5e75340f1a32a50
jinny <dieunn@naver.com>
2023-08-23 11:16:25 +0900

    메인 레이아웃 브라우저 크기에 따라 여백 조정

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="q-mx-xl q-my-lg">
+  <div :class="{'q-mx-lg q-my-lg': browserSize < 600, 'q-mx-xl q-my-lg': browserSize >= 600}">
     <Header></Header>
     <router-view></router-view>
   </div>
 </template>
 
 <script setup>
-
 import Header from "components/Header.vue";
+import {onMounted, ref} from "vue";
+
+const browserSize = ref(window.innerWidth)
+const handleBrowserSize = () => {
+  browserSize.value = window.innerWidth
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleBrowserSize)
+})
+
 </script>
