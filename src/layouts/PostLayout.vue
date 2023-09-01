0c611c746ef9836583afb2bb07d8dacadb928775
jieun <dieunn@naver.com>
2023-09-01 17:28:08 +0900

    게시물 상세페이지 왼쪽

--- src/layouts/PostLayout.vue
+++ src/layouts/PostLayout.vue
@@ -3,9 +3,15 @@
         <q-page-sticky
           v-if="browserSize >= 1300"
           position="left"
-          :offset="[200, 0]"
-          style="border: black solid 1px"
-        >left</q-page-sticky>
+          :offset="[150, 0]"
+        >
+          <div class="column items-center">
+            <q-btn round unelevated outline color="grey-5" icon="fa-solid fa-heart"></q-btn>
+            <div class="">32</div>
+            <q-btn round unelevated outline color="grey-5" icon="fa-solid fa-share-nodes"></q-btn>
+          </div>
+
+        </q-page-sticky>
         <router-view class="content-area"></router-view>
         <q-page-sticky
           v-if="browserSize >= 1300"
@@ -23,8 +29,6 @@ const browserSize = ref(window.innerWidth)
 const handleBrowserSize = () => {
   browserSize.value = window.innerWidth
 }
-
-const leftDrawerOpen = true
 
 onMounted(() => {
   window.addEventListener('resize', handleBrowserSize)
